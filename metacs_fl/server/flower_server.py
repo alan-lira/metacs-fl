e378af75a860dec6417b0b9348bcac301c900e6f
Alan L. Nunes <alan_lira@id.uff.br>
2025-12-04 17:06:40 -0300

    Small fix.

--- metacs_fl/server/flower_server.py
+++ metacs_fl/server/flower_server.py
@@ -1356,9 +1356,10 @@ class FlowerServer(Strategy):
         self._append_round_data_to_history_files(server_round, phase)
         # Store the improved global parameters.
         self._set_attribute("_global_parameters", aggregated_model_parameters)
-        # Update the clients reliability score.
-        completed_clients = {"client_{0}".format(result.metrics["client_id"]): True for _, result in results}
-        self._update_clients_reliability_score_history(server_round, phase, completed_clients)
+        # Update the clients reliability score, if being monitored.
+        if server_strategy_settings.get("monitor_clients_reliability_score", False):
+            completed_clients = {"client_{0}".format(result.metrics["client_id"]): True for _, result in results}
+            self._update_clients_reliability_score_history(server_round, phase, completed_clients)
         # Log an 'end of the aggregate_fit call' debug message.
         message = "[Server {0} | Round {1}] End of the 'aggregate_fit' call!".format(server_id, server_round)
         log_message(logger, message, "DEBUG")
@@ -1513,9 +1514,10 @@ class FlowerServer(Strategy):
             self._initialize_history_output_files(phase)
         # Append the communication round data to the testing history files.
         self._append_round_data_to_history_files(server_round, phase)
-        # Update the clients reliability score.
-        completed_clients = {"client_{0}".format(result.metrics["client_id"]): True for _, result in results}
-        self._update_clients_reliability_score_history(server_round, phase, completed_clients)
+        # Update the clients reliability score, if being monitored.
+        if server_strategy_settings.get("monitor_clients_reliability_score", False):
+            completed_clients = {"client_{0}".format(result.metrics["client_id"]): True for _, result in results}
+            self._update_clients_reliability_score_history(server_round, phase, completed_clients)
         # Log an 'end of the aggregate_evaluate call' debug message.
         message = "[Server {0} | Round {1}] End of the 'aggregate_evaluate' call!".format(server_id, server_round)
         log_message(logger, message, "DEBUG")
